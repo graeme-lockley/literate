1e0a2708b76e8867d488501aabde43d81b244563
Graeme Lockley <graemel@no9.co.za>
2018-12-28 11:44:42 +0200

    feature: basic working copy of make

--- tools/src/main/kotlin/za/co/no9/literate/tools/make/Main.kt
+++ tools/src/main/kotlin/za/co/no9/literate/tools/make/Main.kt
@@ -3,7 +3,37 @@ package za.co.no9.literate.tools.make
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
 import com.xenomachina.argparser.mainBody
+import freemarker.template.Configuration
+import za.co.no9.literate.configure
 import java.io.File
+import java.io.StringWriter
+import java.util.*
+
+
+
+interface Listener {
+    fun processedTemplate(output: String)
+}
+
+
+class MyListener(val log: Log, val showProcessedOutput: Boolean):Listener {
+    override fun processedTemplate(output: String) {
+        if (showProcessedOutput) {
+            log.info(output)
+        }
+    }
+}
+
+
+class MyLog: Log {
+    override fun error(message: String) {
+        System.err.println("Error: $message")
+    }
+
+    override fun info(message: String) {
+        System.out.println(message)
+    }
+}
 
 
 class Arguments(parser: ArgParser) {
@@ -27,16 +57,60 @@ fun main(args: Array<String>): Unit =
             val parsedArgs =
                     ArgParser(args).parseInto(::Arguments)
 
+            val log =
+                    MyLog()
+
+            val listener =
+                    MyListener(log, true)
+
             parsedArgs.run {
-                build(source, target, verbose)
+                build(listener, source, target, verbose)
             }
         }
 
 
-fun build(source: String, target: String, verbose: Boolean) {
+fun build(listener: Listener, source: String, target: String, verbose: Boolean) {
+    val configuration =
+            configure(File("."))
+
+    val sourceFile =
+            File(source)
+
+    val targetFile =
+            File(target)
+
+    targetFile.mkdirs()
+
+    if (sourceFile.isDirectory) {
+        sourceFile.walk().filter { it.isFile && it.name.endsWith("lmd") }.forEach {
+            build(listener, configuration, it, targetFile)
+        }
+    }
+}
+
+
+fun build(log: Log, source: File, target: File) {
 
 }
 
-fun build(log: Log, file: File, file1: File) {
+fun build(listener: Listener, configuration: Configuration, source: File, target: File) {
+    configuration.setDirectoryForTemplateLoading(source.parentFile)
 
+    val template =
+            configuration.getTemplate(source.name)
+
+    val state =
+            mapOf(
+                    Pair("date", Date())
+            )
+
+    val outputWriter =
+            StringWriter()
+
+    template.process(state, outputWriter)
+
+    val output =
+            outputWriter.toString()
+
+    listener.processedTemplate(output)
 }
