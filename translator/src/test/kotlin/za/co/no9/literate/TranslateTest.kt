629332d963ecaea2b2faa1235d8151a38488b6a5
Graeme Lockley <graemel@no9.co.za>
2018-12-28 14:48:19 +0200

    test: add tests to confirm multiple chunks

--- translator/src/test/kotlin/za/co/no9/literate/TranslateTest.kt
+++ translator/src/test/kotlin/za/co/no9/literate/TranslateTest.kt
@@ -22,4 +22,41 @@ class TranslateTest : StringSpec({
                         Pair("Name", listOf(Chunk("Hello World", false, emptyList())))
                 )))
     }
+
+
+    "Content with a multiple chunks with different names" {
+        extractChunks("asdfasdf\n" +
+                "asdfasdf\n" +
+                "~~~ NameA\n" +
+                "Hello World\n" +
+                "~~~~\n" +
+                "some or other test\n" +
+                "~~~ NameB\n" +
+                "Bye bye love\n" +
+                "~~~~\n" +
+                "asdfasdfaf")
+                .shouldBe(Okay<Exception, Map<String, List<Chunk>>>(mapOf(
+                        Pair("NameA", listOf(Chunk("Hello World", false, emptyList()))),
+                        Pair("NameB", listOf(Chunk("Bye bye love", false, emptyList())))
+                )))
+    }
+
+
+    "Content with a multiple chunks with the same name" {
+        extractChunks("asdfasdf\n" +
+                "asdfasdf\n" +
+                "~~~ Name\n" +
+                "Hello World\n" +
+                "~~~~\n" +
+                "some or other test\n" +
+                "~~~ Name\n" +
+                "Bye bye love\n" +
+                "~~~~\n" +
+                "asdfasdfaf")
+                .shouldBe(Okay<Exception, Map<String, List<Chunk>>>(mapOf(
+                        Pair("Name", listOf(
+                                Chunk("Hello World", false, emptyList()),
+                                Chunk("Bye bye love", false, emptyList())))
+                )))
+    }
 })
