57e6688510c5967e273c399c1ff0526c12a20c7b
Graeme Lockley <graemel@no9.co.za>
2018-12-29 12:27:40 +0200

    refactor: move ConsList out into it's own file

--- translator/src/main/kotlin/za/co/no9/literate/Line.kt
+++ translator/src/main/kotlin/za/co/no9/literate/Line.kt
@@ -1,43 +1,6 @@
 package za.co.no9.literate
 
 
-abstract class ConsList<X> {
-    abstract fun <R> foldLeft(z: R, f: (R, X) -> R): R
-
-    abstract fun append(element: X): ConsList<X>
-
-    abstract fun <E, R> rmap(f: (X) -> Result<E, R>): Result<E, ConsList<R>>
-}
-
-
-class Cons<X>(val car: X, val cdr: ConsList<X>) : ConsList<X>() {
-    override fun <R> foldLeft(z: R, f: (R, X) -> R): R =
-            this.cdr.foldLeft(f(z, this.car), f)
-
-    override fun append(element: X): ConsList<X> =
-            Cons(car, cdr.append(element))
-
-    override fun <E, R> rmap(f: (X) -> Result<E, R>): Result<E, ConsList<R>> =
-            this.cdr.rmap(f)
-                    .andThen { cdr ->
-                        f(this.car)
-                                .andThen { car -> Okay<E, ConsList<R>>(Cons(car, cdr)) }
-                    }
-}
-
-
-class Nil<X> : ConsList<X>() {
-    override fun <R> foldLeft(z: R, f: (R, X) -> R): R =
-            z
-
-    override fun append(element: X): ConsList<X> =
-            Cons(element, this)
-
-    override fun <E, R> rmap(f: (X) -> Result<E, R>): Result<E, ConsList<R>> =
-            Okay(Nil())
-}
-
-
 abstract class Line(open val content: String)
 
 
